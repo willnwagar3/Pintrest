cf069865ec33b2eab08818755781d8ee2175dca3
willnwagar3 <willnwagar3@icloud.com>
2015-02-02 19:00:54 -0500

    Create custom migration for users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114234533) do
+ActiveRecord::Schema.define(version: 20150202235413) do
 
   create_table "boards", force: true do |t|
     t.string   "name"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20150114234533) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "last_name"
