5f688ce846750134da0f3eb849be4bf8045cc953
willnwagar3 <willnwagar3@icloud.com>
2015-02-02 19:28:16 -0500

    Update user model, routes, users table, and devise config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	validates :first_name, :presence => true
 	# validates :email, :presence => true
 	has_many :boards
@@ -7,4 +11,4 @@ class User < ActiveRecord::Base
 	def full_name
 	"#{first_name} #{last_name}"
 	end
-end
+end 
